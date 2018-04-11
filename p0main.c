e371b370587d5f2f2030475cb09660a598f321ce
tiffywang92 <tiffywang92@yahoo.com>
2018-04-11 13:07:12 -0700

    Add files via upload

--- p0main.c
+++ p0main.c
@@ -1,42 +1,83 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
+#include "math.h"
 
-#define struct node node
-{
-	char val[100];
+typedef struct node {
+    char val[100];
 	int scopeId;
 	struct node* next;
+} node;
+
+int insert(node* table[], char str[], int scope)
+{
+    if (str == NULL)
+        return -1;
+    /*if (find(table, str, scope))
+        return -2;*/
+
+    node* head, *curr = malloc(sizeof(node));
+    int i, hash, countdown = sizeof(str);
+    //double sum = 0.0;
+    int sum = 0;
+
+    // hash method using the 128-base formula
+//    for (i = 0; i < sizeof(str); i++) {
+//        sum += atof(str[i]) * pow(128.0, (double)countdown);
+//        countdown--;
+//    }
+
+    // alt hash method
+//    for (i = 0; i < sizeof(str); i++) {
+//        sum += atof(str[i]) * pow(31.0, (double)countdown);
+//        countdown--;
+//    }
+    for (i = 0; i < sizeof(str); i++) {
+        sum += str[i] - '\0';
+    }
+    hash = (int)sum % 31;
+
+    // insert the string
+    head = table[hash];
+    if (head == NULL) {
+        head = curr;
+        head->next = NULL;
+    }
+    else {
+        head->next = curr;
+        curr->next = NULL;
+    }
+    strcpy(head->val, str);
+    head->scopeId = scope;
+
+    // insert successful
+    return 0;
 }
 
 int main()
 {
-	FILE* fin = fopen("p0input.txt");
-	bool curr = false;
+    //FILE* fin = fopen("p0input.txt", "r");
+    node* symTable[31];
+	//bool curr = false;
 	int i, j;
-	
-	while (fscanf(fin, "%s", str) != EOF)
-	{
-		if (strcmp(str, "{") == 0)
-		{
-			// start a block
-		}
-		else if (strcmp(str, "}") == 0)
-		{
-			// close a block
-		}
-		else	// string looks like an id
-		{
-			curr = find_in_current(node.val, node.scopeId);
-			if (!curr)
-			{
-				insert(node.val, node.scopeId);
-				find_in_all(node.val)	// to check if anywhere else in the table
-			}
-		}
+
+	for (i = 0; i < 31; i++) {
+        symTable[i] = NULL;
 	}
-	printf("Symbol Table\n");
-	for (i = 0; i < 3; i++)
-	{
-		// print scope block 1, 2, 3...
-	}
+
+    // loop through input file
+
+
+    // test insert one
+    if (insert(symTable, "hello", 0) == 0)
+        printf("Inserted.\n");
+
+    // print out values
+    for (i = 0; i < 31; i++) {
+        if (symTable[i] != NULL) {
+            printf("%s, %d\n", symTable[i]->val, symTable[i]->scopeId);
+        }
+    }
+
+	return 0;
 }
