552c4f801827320a806723b3b3412bb2c664a66c
ImAsFreeAsMyHair <lawrence2055@gmail.com>
2018-04-13 13:00:17 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ void pop()
 
 typedef struct node {
     char val[100];
-	int scopeId;
-	struct node* next;
+    int scopeId;
+    struct node* next;
 } node;
 
 const int tableSize = 31;
@@ -29,129 +29,113 @@ const int tableSize = 31;
 void displayScopes(struct node* array[], int arraySize, int highestScope)
 {
     struct node* nodeptr;
-      int i;
-      /*
-      int highestScope = 0;
-      for(i = 0; i < arraySize; i++)
-      {
-          if(array[i] != NULL)
-          {
-              nodeptr = array[i];
-              while(nodeptr != NULL)
-              {
-                  if(nodeptr->scopeId > highestScope)
-                  {
-                      highestScope = nodeptr->scopeId;
-                  }
-                  nodeptr = nodeptr->next;
-              }
-          }
-      }
-       */
-      
-      int j;
-      int k;
-      for(j = 0; j < highestScope + 1; j++)
-      {
-          printf("Scope %d:", j);
+    int i, j, k;
+    for(j = 0; j < highestScope + 1; j++)
+    {
+        printf("Scope %d:", j);
           
-          for(k = 0; k < arraySize; k++)
-          {
-              if(array[k] != NULL)
-              {
-                  nodeptr = array[k];
-                  while(nodeptr != NULL)
-                  {
-                      if(nodeptr->scopeId == j)
-                      {
-                      printf("%s ", nodeptr->val);
-                      }
-                      nodeptr = nodeptr->next;
-                  }
-              }
-          }
-          
-          printf("\n");
-      }
-      
-      
-      
+        for(k = 0; k < arraySize; k++)
+        {
+            if(array[k] != NULL)
+            {
+                nodeptr = array[k];
+                while(nodeptr != NULL)
+                {
+                    if(nodeptr->scopeId == j)
+                    {
+                        printf("%s ", nodeptr->val);
+                    }
+                    nodeptr = nodeptr->next;
+                }
+            }
+        }
+        printf("\n");
+    }  
 }
 
 void display(struct node* array[], int arraySize)
 {
-      struct node* nodeptr;
-      int i;
+    struct node* nodeptr;
+    int i;
 
-      for(i = 0; i < arraySize; i++)
-      {
-            printf("[%d]", i);
-            if(array[i] != NULL)
+    for(i = 0; i < arraySize; i++)
+    {
+        printf("[%d]", i);
+        if(array[i] != NULL)
+        {
+            nodeptr = array[i];
+            while(nodeptr != NULL)
             {
-                  nodeptr = array[i];
-                  while(nodeptr != NULL)
-                  {
-                        printf("[%s %d] ", nodeptr->val, nodeptr->scopeId);
-
-                        nodeptr = nodeptr->next;
-                  }
+                printf("[%s %d] ", nodeptr->val, nodeptr->scopeId);
+                nodeptr = nodeptr->next;
             }
-            printf("\n");
+        }
+        printf("\n");
       }
 }
 
 void find_in_all_scopes(struct node* array[], int arraySize, char str[])
 {
-      struct node* nodeptr;
-      int i;
-
-      for(i = 0; i < arraySize; i++)
-      {
-            printf("[%d]", i);
-            if(array[i] != NULL)
+    struct node* nodeptr;
+    int i;
+    for(i = 0; i < arraySize; i++)
+    {
+        printf("[%d]", i);
+        if(array[i] != NULL)
+        {
+            nodeptr = array[i];
+            while(nodeptr != NULL)
             {
-                  nodeptr = array[i];
-                  while(nodeptr != NULL)
-                  {
-                        if(!strcmp(nodeptr->val, str))
-                        {
-                            printf("[%s %d] ", nodeptr->val, nodeptr->scopeId);
-                        }
-
-                        nodeptr = nodeptr->next;
-                  }
+                if(!strcmp(nodeptr->val, str))
+                {
+                    printf("[%s %d] ", nodeptr->val, nodeptr->scopeId);
+                }
+                nodeptr = nodeptr->next;
+                }
             }
             printf("\n");
       }
 }
 
 int find_in_current_scope(struct node* array[], int arraySize, int currentScope, char str[])
-{
-      struct node* nodeptr;
-      int i, index = 0;
-
-      for(i = 0; i < arraySize; i++)
-      {
-            if(array[i] != NULL)
+{   
+    struct node* nodeptr;
+    int i, index = 0, hash;
+    hash = hashMethod(str);
+    nodeptr = array[hash];
+    while(nodeptr != NULL)
+    {
+        ++index;
+        if(nodeptr->scopeId == currentScope)
+        {
+            if(strcmp(nodeptr->val, str) == 0)
             {
-                  nodeptr = array[i];
-                  while(nodeptr != NULL)
-                  {
-                      ++index;
-                        if(nodeptr->scopeId == currentScope /*&& !strcmp(nodeptr->val, str)*/)
-                        {
-                            if(strcmp(nodeptr->val, str) == 0)
-                            {
-                                return index;   // place along linked list
-                            }
-                        }
-
-                        nodeptr = nodeptr->next;
-                  }
+                return index;
             }
+        }
+        nodeptr = nodeptr->next;
+    }
+    
+}
 
-      }
-      return 0;
+int hashMethod(char str[])
+{
+    int i, j, hash;
+    int length = sizeof(str);
+    int sum = 0;
+    int product = 1;
+    
+    for (i = 0; i < length; i++)
+    {
+        for (j = length; j > 0; j--)
+        {
+            product *= 128;
+        }
+        sum += (str[i] - '\0') * product;
+    }
+    
+    hash = (int)sum % 31;
+    return hash;
 }
 
 int insert(node* table[], int scope, char str[])
@@ -162,30 +146,15 @@ int insert(node* table[], int scope, char str[])
 
     node* head;                         // to mark head of a chain
     node* curr = malloc(sizeof(node));  // to mark new node
-    int i, hash;
+    int i, j, hash;
+    int length = sizeof(str);           // length of string
     int sum = 0;                        // hold a sum for hash
-    //int countdown = sizeof(str);
-    //double sum = 0.0;
+    int product = 1;                    // hold a product for exponents
 
-    // hash method using the 128-base formula
-//    for (i = 0; i < sizeof(str); i++) {
-//        sum += atof(str[i]) * pow(128.0, (double)countdown);
-//        countdown--;
-//    }
 
-    // alt hash method
-//    for (i = 0; i < sizeof(str); i++) {
-//        sum += atof(str[i]) * pow(31.0, (double)countdown);
-//        countdown--;
-//    }
-
-    // working hash method
-    for (i = 0; i < sizeof(str); i++) {
-        sum += str[i] - '\0';
-    }
-    hash = (int)sum % 31;
-    //printf("hash: %d\n", hash);
-
+    hash = hashMethod(str);
+    
+    
     // insert the string
     head = table[hash];
     if (head == NULL) {
@@ -208,30 +177,18 @@ int main()
 {
     FILE* fin;                  // input file
     node* symTable[31];         // hash table/array
-	int i;                      // increment
-	int scopeCount = 0;         // counter for scopes
-	int excep;                  // to handle exceptions
-	char buffer[100];           // string to hold read-in data
+    int i;                      // increment
+    int scopeCount = 0;         // counter for scopes
+    int excep;                  // to handle exceptions
+    char buffer[100];           // string to hold read-in data
 
-	fin = fopen("p0input.txt", "r");
+    fin = fopen("p0input.txt", "r");
 
-	// initialize all pointers in table
-	for (i = 0; i < 31; i++) {
+    // initialize all pointers in table
+    for (i = 0; i < 31; i++)
+    {
         symTable[i] = NULL;
-	}
-
-    // test insert one
-//    insert(symTable, "hello", 0);
-//    insert(symTable, "hello", 1);
-//    insert(symTable, "ayy", 0);
-//
-//    for (i = 0; i < 31; i++) {
-//        while (symTable[i] != NULL) {
-//            printf("val: %s\n", symTable[i]->val);
-//            printf("scope: %d\n", symTable[i]->scopeId);
-//            symTable[i] = symTable[i]->next;
-//        }
-//	}
+    }
 
     // loop through input file
     while (fscanf(fin, "%s", buffer) != EOF) {  // while not end of file
@@ -261,9 +218,9 @@ int main()
         }
     }
 
-    // print out symbol tablehttps://mail.google.com/mail/u/1/#inbox
+    // print out symbol table
     displayScopes(symTable, tableSize, stackCount);
     //display(symTable, tableSize);
 
-	return 0;
+    return 0;
 }
